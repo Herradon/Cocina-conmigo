1b47b35f7e84282817ea2124c0703f9de76243b6
Herradon <73962361+Herradon@users.noreply.github.com>
2023-11-29 18:14:03 +0100

    cambios en el carrousel

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*--CARROUSEL1--*/
+
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const CARROUSEL1 = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
+
+/*--CARROUSEL2--*/
+
+
+const CARROUSEL2 = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})

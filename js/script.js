99df2d7bc0f0c9c7852a60ed9e9ca1a14f088793
Herradon <73962361+Herradon@users.noreply.github.com>
2023-11-29 18:51:22 +0100

    cambios en los botones

--- js/script.js
+++ js/script.js
@@ -2,15 +2,7 @@
 
 const myCarouselElement = document.querySelector('#myCarousel')
 
-const CARROUSEL1 = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: false
-})
-
-/*--CARROUSEL2--*/
-
-
-const CARROUSEL2 = new bootstrap.Carousel(myCarouselElement, {
+const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false
 })
